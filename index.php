068c17f12949bdc79eb92e0491553456e32fd7ab
João Victor Garcia <joao.garcia18@fatec.sp.gov.br>
2021-10-23 16:58:07 -0300

    update index page

--- index.php
+++ index.php
@@ -23,33 +23,6 @@
 
     <!-- MAIN NAVBAR ======================= -->
     <?php include 'navbar-main.php'; ?>
-    <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid px-md-5">
-            <button class="col-md-1 navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarToggleMain" aria-controls="navbarToggleMain" aria-expanded="true"
-                aria-label="Menu de navegação">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <a class="col-8 col-md-4 navbar-brand" href="index.html">
-                <img class="img-fluid logo-img" src="img/svg/main_logo_v0.svg"
-                    alt="Logo principal do portal de notícias InTimeNews">
-            </a>
-            <div class="collapse navbar-collapse row" id="navbarToggleMain">
-                <img src="img/just_example/banner-750x150.jpg" alt="Banner de exemplo" class="img-fluid banner">
-                <ul class="navbar-nav mb-2 mb-lg-0 justify-content-end">
-                    <li class="nav-item ms-3">
-                        <a class="nav-link" href="#">Noticías</a>
-                    </li>
-                    <li class="nav-item ms-3">
-                        <a class="nav-link" href="#">Informações gerais</a>
-                    </li>
-                    <li class="nav-item ms-3">
-                        <a class="nav-link" href="#">Area social</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav> -->
 
     <div class="m-5">
         <h1 class="m-5">Content</h1>
@@ -58,96 +31,6 @@
     
     <!-- MAIN FOOTER ======================= -->
     <?php include 'footer-main.php' ?>
-    <!-- <footer class="page-footer font-small py-3 px-1 bg-light">
-        <div class="container-fluid row">
-            <div class="col-lg-4 row">
-                <a class="col-lg-6" href="index.html">
-                    <img class="img-fluid logo-img" src="img/svg/relogio_logo.svg"
-                        alt="Logo principal do portal de notícias InTimeNews">
-                </a>
-                <div class="col-lg-6 text-logo">
-                    <p>In Time</p>
-                    <p>Notícias</p>
-                </div>
-            </div>
-            <div class="col-lg-4 row">
-                <div class="col-lg-12">
-                    <h5 class="font-weight-bold text-uppercase mb-2">Notícias</h5>
-
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="#!">CPI da COVID-19</a>
-                        </li>
-                        <li>
-                            <a href="#!">Sintomas e tratamentos</a>
-                        </li>
-                        <li>
-                            <a href="#!">Taxa de infectados</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-lg-12 mt-4">
-                    <h5 class="font-weight-bold text-uppercase mb-2">Informações Gerais</h5>
-
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="#!">Variantes da COVID-19</a>
-                        </li>
-                        <li>
-                            <a href="#!">Informações</a>
-                        </li>
-                        <li>
-                            <a href="#!">Curiosidades</a>
-                        </li>
-                        <li>
-                            <a href="#!">Link 4</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-4 rowr">
-                <div class="col-lg-12">
-                    <h5 class="font-weight-bold text-uppercase mb-2">Nossa area social</h5>
-
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="#!">Termos de uso</a>
-                        </li>
-                        <li>
-                            <a href="#!">Contato</a>
-                        </li>
-                        <li>
-                            <a href="#!">Quem somos</a>
-                        </li>
-                    </ul>
-                </div>
-                <form action="#" method="get" class="col-lg-12">
-                    <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1"
-                            placeholder="name@example.com">
-                    </div>
-                </form>
-                <ul class="icons list-unstyled row text-center">
-                    <li class="col">
-                        <a href="#!"><i class="bi bi-linkedin"></i>
-                        </a>
-                    </li>
-                    <li class="col">
-                        <a href="#!"><i class="bi bi-twitter"></i></a>
-                    </li>
-                    <li class="col">
-                        <a href="#!"><i class="bi bi-facebook"></i></a>
-                    </li>
-                    <li class="col">
-                        <a href="#!"><i class="bi bi-instagram"></i></a>
-                    </li>
-                </ul>
-
-            </div>
-        </div>
-
-    </footer> -->
 
     <!-- JS BOOTSTRAP 5 -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
