ed64f769fc8d6e919cc9ccda16c1f6b04b76dea3
João Victor Garcia <joao.garcia18@fatec.sp.gov.br>
2021-11-08 16:24:02 -0300

    update on css File. version 0.0.2

--- index.php
+++ index.php
@@ -28,85 +28,73 @@
     <!-- PAGE CONTENT -->
     <main class="container-xxl">
         <!-- ================
-        section-news -->
-        <section class="section-news-content mx-auto row">
+        article-news -->
+        <article class="article-news-content mx-auto row">
+            <h1>Ultimas Notícias</h1>
             <div class="initial-news col-sm-12 col-md-12 col-lg-6">
-                <article class="article-news">
+                <section class="section-news">
+                    <img src="img/just_example/news-250x250.jpg" class="img-fluid" alt="Imagem da notícia principal">
+                    <div class="small-text">
+                        <span class="under-img-text col">25/10/2021</span>
+                        <span class="under-img-text col">tempo de leitura: 5 minutos</span>
+                    </div>
                     <header class="news-header">
-                        <h1>Notícia principal da pag</h1>
-                        <div class="news-img row">
-                            <img src="img/just_example/news-250x250.jpg" class="img-fluid col-12" alt="Imagem da notícia principal">
-                            <span class="under-img-text col">25/10/2021</span>
-                            <span class="under-img-text col">tempo de leitura: 5 minutos</span>
-                        </div>
+                        <h2>Notícia principal da pag</h2>
+                        <p class="news-text">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum fugiat dolore sapiente vitae doloribus.
+                        </p>
                     </header>
-                    <p class="news-text">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum fugiat dolore sapiente vitae doloribus.
-                    </p>
-                </article>
+                </section>
             </div>
             
-            <div class="aside-news col-sm-12 col-md-12 col-lg-6 row">
-                <article class="article-news col-12">
-                    <header class="news-header">
-                        <h2>Notícia principal da pag</h2>
-                        <div class="news-img row">
-                            <img src="img/just_example/news-250x250.jpg" class="img-fluid col-12" alt="Imagem da notícia principal">
+            <div class="aside-news col-sm-12 col-md-12 col-lg-6">
+                <div class="row">
+                    <section class="section-news col-12">
+                        <img src="img/just_example/news-250x250.jpg" class="img-fluid col-12" alt="Imagem da notícia principal">
+                        <div class="small-text">
                             <span class="under-img-text col">25/10/2021</span>
                             <span class="under-img-text col">tempo de leitura: 5 minutos</span>
                         </div>
-                    </header>
-                    <p class="news-text">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum fugiat dolore sapiente vitae doloribus.
-                    </p>
-                </article>
+                        <header class="news-header">
+                            <h2>Notícia principal da pag</h2>
+                            <p class="news-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum fugiat dolore sapiente vitae doloribus.
+                            </p>
+                        </header>
+                    </section>
 
-                <article class="article-news col-sm-12 col-md-6 col-lg-6">
-                    <header class="news-header">
-                        <h2>Notícia principal da pag</h2>
-                        <div class="news-img row">
-                            <img src="img/just_example/news-250x250.jpg" class="img-fluid col-12" alt="Imagem da notícia principal">
+                    <section class="section-news col-sm-12 col-md-6">
+                        <img src="img/just_example/news-250x250.jpg" class="img-fluid col-12" alt="Imagem da notícia principal">
+                        <div class="small-text">
                             <span class="under-img-text col">25/10/2021</span>
                             <span class="under-img-text col">tempo de leitura: 5 minutos</span>
                         </div>
-                    </header>
-                    <p class="news-text">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum fugiat dolore sapiente vitae doloribus.
-                    </p>
-                </article>
+                        <header class="news-header">
+                            <h2>Notícia principal da pag</h2>
+                            <p class="news-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                        </header>
+                    </section>
 
-                <article class="article-news col-sm-12 col-md-6 col-lg-6">
-                    <header class="news-header">
-                        <h2>Notícia principal da pag</h2>
-                        <div class="news-img row">
-                            <img src="img/just_example/news-250x250.jpg" class="img-fluid col-12" alt="Imagem da notícia principal">
+                    <section class="section-news col-sm-12 col-md-6">
+                        <img src="img/just_example/news-250x250.jpg" class="img-fluid col-12" alt="Imagem da notícia principal">
+                        <div class="small-text">
                             <span class="under-img-text col">25/10/2021</span>
                             <span class="under-img-text col">tempo de leitura: 5 minutos</span>
                         </div>
-                    </header>
-                    <p class="news-text">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum fugiat dolore sapiente vitae doloribus.
-                    </p>
-                </article>
+                        <header class="news-header">
+                            <h2>Notícia principal da pag</h2>
+                            <p class="news-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                        </header>
+                    </section>
+                </div>
             </div>
-        </section>
-        <!-- section-news
+        </article>
+        <!-- article-news
         ================ -->
-
-        <!-- <section class="section-personal-pages">
-            <h2>Informações gerais</h2>
-            <div class="personal-page-card row">
-                <div class="col-6">
-                    <img class="img-fluid" src="img/just_example/news-250x250.jpg" alt="Imagem das paginas pessoais">
-                </div>
-                <div class="personal-page-text col-6">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque nihil autem ab quod impedit velit dicta ducimus, laudantium.</p>
-                </div>
-                <div class="under-card col-12 row">
-                    <span class="col">25/10/2021</span><span class="col">João Victor Garcia</span>
-                </div>
-            </div>
-        </section> -->
 
     </main>
     
