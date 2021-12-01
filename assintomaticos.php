ee3e8a0324e97b6d150a72a25b4b1b4b68f53d8e
Guimonteirojbv <guilhermemonteiroro@gmail.com>
2021-11-30 23:13:41 -0300

    page assintomáticos

--- assintomaticos.php
+++ assintomaticos.php
@@ -14,7 +14,7 @@
 
         <!-- CUSTOM CSS -->
         <link rel="stylesheet" href="css/main-page.css">
-        <link rel="stylesheet" href="css/info.css">
+        <link rel="stylesheet" href="css/assintomaticos.css">
 
 
       <title>Informaçoes</title>
@@ -27,31 +27,85 @@
 
         <main class="container-xxl">
     
+        <div class=" col-lg-12 col-md-12 col-sm-12">
+          <header class="col-lg-9 col-md-9 col-sm-12 mx-auto">
+            <h1 class="title-vacinas">A vacinação é talvez o momento mais importante na batalha contra uma doença que devasta a população mundial, e não devemos nunca negar à essa ferramenta de imunização.</h1>
+            <h2 class="text-lora mt-3 mb-5">O Brasil já alcançou um total de 62,9% de vacinados - numeros tirados de "Our World In Data".</h2>
+            <p>Fontes: https://www.pfizer.com.br/sua-saude/vacinacao/tudo-sobre-vacinas </p>
+          </header>
+        </div>
 
-      <!--Section com conteúdo-->
+      <!--Section com início do conteúdo-->
 
-      <section class="mx-auto row col-lg-12 col-md-12 col-sm-12">
-        <article class="col-lg-6 col-md-6 col-sm-12">
-          <h1 class="big-title" >TESTANDO</h1>
-          <p class="text-lora">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, nesciunt rerum amet tempore corporis labore ipsum dolorum nostrum velit magni quae sint vitae cum blanditiis quisquam laborum numquam? Non, explicabo?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, nesciunt rerum amet tempore corporis labore ipsum dolorum nostrum velit magni quae sint vitae cum blanditiis quisquam laborum numquam? Non, explicabo?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, nesciunt rerum amet tempore corporis labore ipsum dolorum nostrum velit magni quae sint vitae cum blanditiis quisquam laborum numquam? Non, explicabo?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, nesciunt rerum amet tempore corporis labore ipsum dolorum nostrum velit magni quae sint vitae cum blanditiis quisquam laborum numquam? Non, explicabo?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, nesciunt rerum amet tempore corporis labore ipsum dolorum nostrum velit magni quae sint vitae cum blanditiis quisquam laborum numquam? Non, explicabo?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, nesciunt rerum amet tempore corporis labore ipsum dolorum nostrum velit magni quae sint vitae cum blanditiis quisquam laborum numquam? Non, explicabo?
+      <section class="mt-5 mx-auto row col-lg-12 col-md-12 col-sm-12">
+        <article class=" mx-auto me-0 col-lg-6 col-md-6 col-sm-12">
+          <h2 class="big-title" >Assintomáticos</h2>
+          <p class="text-lora">Os pacientes considerados assintomáticos são aqueles que testam positivo para o vírus, porém, não apresentam nenhum dos sintomas durante todo o período de desenvolvimento da doença. A epidemiologista da OMS baseou a sua afirmação em dados de alguns países que afirmam não terem casos de transmissão da doença por pessoas assintomáticas. 
+          Entretanto, os órgãos de saúde alertam que ainda não há um consenso sobre o levantamento. Por se tratar de um vírus novo, com poucos dados concretos sobre ele, é fundamental ter cautela nas ações para não estimular ainda mais a transmissão da doença. 
           </p>
         </article>
+        <aside class="ms-lg-0 ms-md-0 ms-sm-auto mx-auto row col-lg-3 col-md-3 col-sm-10">
+          <img class="img-fluid" src="img/imagem-informacoes/aside-assintomaticos.jpg" alt="Mulher protegida da covid">
+        </aside>
+        </section>
+
+        <!--Section com o restante do conteúdo-->
+        <div class="mx-auto row col-lg-12 col-md-12 col-sm-12">
+          <section class="assintomaticos-info mx-auto mt-4 col-lg-8 col-md-8 col-sm-12" >
+          <h2 class="big-title">Pré-sintomáticos</h2>
+          <p class="text-lora">Os pré-sintomáticos são pessoas com cargas virais bastante altas, mas que ainda não apresentam sintomas. Por conta disso, o grupo dos pré-sintomáticos é considerados aquele com maior potencial de transmissão do vírus. De acordo com uma pesquisa do Núcleo em Ecologia e Desenvolvimento Sócio-Ambiental de Macaé (NUPEM), da UFRJ, essas pessoas são representam cerca de 45% das transmissões. Ou seja, transmitem mais o vírus do que os sintomáticos e os assintomáticos. 
+
+          Segundo os estudiosos, isso acontece porque os pré-sintomáticos possuem as cargas virais altas e não percebem que estão com sintomas ou acreditam que não podem transmitir por não apresentarem sintomas. 
+
+          Por isso, para determinar se o paciente é assintomático é necessário aguardar todo o período de desenvolvimento da doença. Antes de finalizar esse período, é possível manifestar os sintomas a qualquer momento. </p>
+          <ul class=" my-5">
+                <li>  
+                    <strong>Leia também:</strong> <a href="assintomaticos.php"> Assintomáticos pré-sintomáticos e sintomáticos são classificações da Organização Mundial da Saúde (OMS) para se referir às pessoas contaminadas pelo novo coronavírus.</a>
+                </li> 
+          </ul>
+    
+
+          <h2 class="big-title">sintomáticos</h2>
+          <p class="text-lora">Já os sintomáticos apresentam os sintomas da doença durante o seu desenvolvimento. Os sintomáticos são responsáveis por 40% da transmissão da doença e correspondem a 80% dos contaminados pelo novo coronavírus. Os dados também são da pesquisa do Nupem, da UFRJ. 
+
+          Os sintomáticos acabam sendo os mais monitorados, pois muitos países preferem acompanhar os sintomáticos por não possuírem condições de testar grande parte da população.</p>
+
+          <p class="fonte"><strong>Fonte:</strong> E+B Educação | Gabriele Silva | https://www.educamaisbrasil.com.br/educacao/dicas/assintomaticos-presintomaticos-e-sintomaticos-entenda-a-diferenca?gclid=cj0kcqiatjenbhcvarisanjuj2fvid9lbkdavurnsxltck80ezbx-delcuodnisf8rhqvoqhj_r-5imaasnoealw_wcb</p>
+          </section>
+          </div>
+
+       
+          <!--Section com notícias externas-->
+
+          
+            <div class=" row col-lg-12 col-md-12 col-sm-12 mx-auto news-externals">
+              <section class="row mx-auto py-3 ms-5 col-lg-12">
+                <div class=" col-lg-6 ">
+                  <img class=" ms-1 img-fluid float-start" src="img/imagem-informacoes/renan-calheiros-sm.png" alt="Imagem de Renan Calheiros">
+                  <div class="ms-1 row float-start col-lg-2 ">
+                    <h1 >CPI COVID</h1>
+                    <a class="text-lora "href="$">Relatório impede indiciamento de Bolsonaro.</a>
+                  </div>
+                </div>
+                <div class="border-start border-3 border-danger ms-0  col-lg-5 flex-end">
+                  <img class="ms-2 img-fluid float-start pr-3" src="img/imagem-informacoes/renan-calheiros-sm.png" alt="Imagem de Renan Calheiros">
+                  <h1 >teste</h1>
+                </div>
+              </section>
+            </div>
+        
+            </main>
+
+       
     
     
-     
-      </section>
 
 
       
       
       
       
-      </main>
+      
 
 
         <!-- MAIN FOOTER ======================= -->
