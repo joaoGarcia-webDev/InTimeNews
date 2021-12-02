391201f6c94ca1edd923cbca16333db3ee2d1f62
João Victor Garcia <joao.garcia18@fatec.sp.gov.br>
2021-12-02 16:44:01 -0300

    contact page

--- contact.php
+++ contact.php
@@ -31,10 +31,10 @@
         <section class="row mail-content">
             <div class="text-content col-sm-12 col-md-6 col-lg-4 offset-lg-2">
                 <div class="header">
-                    <h1 class="big-title">Cadastre-se e receba as ultimas notícias por e-mail</h1>
-                    <p class="my-4">Finalizando o cadastro você passa a receber, de forma gratuita, todas as notícias diretamente no seu e-mail, dessa forma você fica por dentro de todos os acontecimentos sobre a pandemia da COVID-19</p>
+                    <h1 class="big-title">Entre em contato com nossa equipe</h1>
+                    <p class="my-4">Nós do Portal de Notícias In Time News estamos sempre a disposição para um contato mais proximo entre nossos usuários, disponibilizando nosso e-mail e contato para caso de dúvidas, sugestões ou elogios</p>
                 </div>
-                <div class="last-text">
+                <div class="last-text-contact">
                     <p>Não utilizamos essas informações para nada além do serviço prestado. Para mais informações acesse <a href="normas.php">Termos de Uso</a>.</p>
                 </div>
             </div>
@@ -43,24 +43,25 @@
                 <form>
                     <div class="mb-3">
                         <label for="name" class="form-label">Nome</label>
-                        <input type="text" value="<?php echo $name; ?>" class="form-control" id="name">
+                        <input type="text" class="form-control" id="name">
                     </div>
                     <div class="mb-3">
                         <label for="email" class="form-label">E-mail</label>
                         <input type="email"
-                        value="<?php echo $email; ?>"
                         class="form-control"
                         id="email"
                         aria-describedby="subEmail">
-                        <div id="subEmail" class="form-text">As notícias serão enviadas para este e-mail</div>
+                        <div id="subEmail" class="form-text">Em caso de resposta por parte da equipe, o email sera enviado para este endereço</div>
                     </div>
                     <div class="mb-3">
-                        <label for="select" class="form-label">Quais notícias deseja receber?</label>
-                        <select id="select" class="form-select">
-                            <option>Todas</option>
-                            <option>CPI - COVID-19</option>
-                            <option>Sintomas e Tratamentos</option>
-                        </select>
+                        <label for="email" class="form-label">confirme o E-mail</label>
+                        <input type="email"
+                        class="form-control"
+                        id="email">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="textArea">Qual sua mensagem?</label>
+                        <textarea class="form-control" placeholder="Digite sua mensagem aqui." id="textArea" style="height: 100px"></textarea>
                     </div>
                     
                     <div class="mb-3 form-check">
@@ -71,6 +72,10 @@
                 </form>
             </div>
         </section>
+
+        <div class="map-content my-5">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.9879122680104!2d-47.13622688447634!3d-23.5329371664296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf0d54817a1725%3A0x3dcff522a27b9b96!2sFaculdade%20de%20Tecnologia%20de%20S%C3%A3o%20Roque!5e0!3m2!1spt-BR!2sbr!4v1638474162602!5m2!1spt-BR!2sbr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
     </main>
     
 
